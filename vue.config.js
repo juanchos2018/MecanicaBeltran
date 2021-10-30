659c9a7654009b0459ced9f61685ce056781affb
juanchos2018 <jcarlossenati@gmail.com>
2021-10-30 14:43:18 -0500

    mecania

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
+let publicPath = process.env.NODE_ENV === 'production' ? '/MecanicaBeltran' : '/';
 module.exports = {
+    publicPath,
     lintOnSave: false,
     runtimeCompiler: true,
     configureWebpack: {
